8ca2b6390bd4693c84916b75dfbc15084b4d2a85
Bedirhan Eren <163941707+bedirhaneren@users.noreply.github.com>
2024-12-22 20:04:44 +0300

    Add files via upload

--- consumer.py
+++ consumer.py
@@ -1,43 +1,65 @@
 import pika
+import json
+import time
 
 WORKING = "working"
 STOPPED = "stopped"
 
 current_state = WORKING
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-
-channel.queue_declare(queue='messageBox')
-
 def callback(ch, method, properties, body):
     
-    
-    current_state
+    global current_state
     if current_state == WORKING:
-        print(f"Received message: {body.decode()}")
+        received_data = json.loads(body.decode())
+        print(f"Takım Numarası : ",received_data['takim_numarasi']) 
+        print(f"IHA Enlem : ",received_data['iha_enlem'])
+        print(f"IHA Boylam : ",received_data['iha_boylam'])
+        print(f"IHA Irtifa : ",received_data['iha_irtifa'])
+        print(f"IHA Dikilme : ",received_data['iha_dikilme'])
+        print(f"IHA Yonelme : ",received_data['iha_yonelme'])
+        print(f"IHA Yatis : ",received_data['iha_yatis'])
+        print(f"Zaman Farki : ",received_data['zaman_farki'])
+        print("\n\n\n")
     else:
         print("Consumer is stopped.")
 
-channel.basic_consume(queue='messageBox', on_message_callback=callback, auto_ack=True)
 
-def set_state(new_state):
+def set_state(channel,new_state):
    
-    current_state
+    global current_state
     current_state = new_state
     print(f"State changed to: {new_state.upper()}")
+    channel.basic_publish(exchange='', routing_key='stateBox', body=new_state)
 
-print("Waiting for messages. To exit press CTRL+C or enter 'STOP' to pause.")
-try:
-    while True:
-        if current_state == WORKING:
-            connection.process_data_events(time_limit=1)
-        else:
-            user_input = input("Enter 'START' to resume or CTRL+C to exit: ").strip().upper()
-            if user_input == "START":
-                set_state(WORKING)
-except KeyboardInterrupt:
-    print("Exiting...")
-finally:
-    connection.close()
-    print("Connection closed.")
+def start():
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='messageBox')
+    channel.queue_declare(queue='stateBox')
+
+    channel.basic_consume(queue='messageBox', on_message_callback=callback, auto_ack=True)
+    print(f"Initial State: {current_state.upper()}")
+    print("Waiting for messages. To exit press CTRL+C or enter 'STOP' to pause.")
+    try:
+        while True:
+            if current_state == WORKING:
+                connection.process_data_events(time_limit=1)
+                
+            else:
+                
+                user_input = input("Enter 'START' to resume or CTRL+C to exit: ").strip().upper()
+                
+                if user_input == "START":
+                    set_state(channel,WORKING)
+    except KeyboardInterrupt:
+        print("Exiting...")
+        set_state(channel,STOPPED)
+        print(current_state)
+        time.sleep(2)
+    finally:
+        connection.close()
+        print("Connection closed.")
+
+if __name__ == '__main__' : 
+    start()
--- producer.py
+++ producer.py
@@ -3,17 +3,67 @@ import pika.connection
 import time
 import random
 import string
+import json
+
+
+class Data: 
+    takim_numarasi = 0
+    iha_enlem = 0
+    iha_boylam = 0
+    iha_irtifa = 0
+    iha_dikilme = 0 
+    iha_yonelme = 0
+    iha_yatis = 0 
+    zaman_farki = 0
+
+def getData(): 
+    data = Data()
+    data.takim_numarasi = random.randint(1, 50)
+    data.iha_enlem = random.randint(0, 1000)
+    data.iha_boylam = random.randint(0, 1000)
+    data.iha_irtifa = random.randint(0, 1000)
+    data.iha_dikilme = random.randint(0, 500)
+    data.iha_yonelme = random.randint(0, 500)
+    data.iha_yatis = random.randint(0, 500)
+    data.zaman_farki = random.randint(0, 50)
+    return data
+
+def dataToJson(data): 
+    return json.dumps(data, default=lambda o: o.__dict__)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue='messageBox')
+channel.queue_declare(queue='stateBox')
 
-while(1) : 
-        length = random.randint(5, 15)  
-        random_message = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+current_state = "working"
 
-        channel.basic_publish(exchange='', routing_key='messageBox', body=random_message)
-        print(f"Message sent: {random_message} ")
-        time.sleep(1)
-connection.close()
+def checkState(): 
+    global current_state
+    method, properties, body = channel.basic_get(queue='stateBox', auto_ack=True)
+    if body:
+        new_state = body.decode()
+        if new_state != current_state:
+            current_state = new_state
+            print(f"State updated: {current_state}")
+
+try:
+    while True:
+        checkState()  
+
+        if current_state == "working":
+            data = getData()
+            iha_veri = dataToJson(data)
+            channel.basic_publish(exchange='', routing_key='messageBox', body=iha_veri)
+            print(f"Current State: {current_state}")
+            print(f"Message sent: {iha_veri}")
+            time.sleep(1)
+        else:
+            print(f"Current State: {current_state} - Data send stopped.")
+            time.sleep(1)
+except KeyboardInterrupt:
+    print("Exiting producer...")
+finally:
+    connection.close()
+    print("Connection closed.")
